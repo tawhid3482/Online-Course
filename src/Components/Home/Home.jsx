2d95adcfe77fd5f2d837a54ad243ce1ddc5ae44f
saikat <tawhidulislam3482@gmail.com>
2023-09-15 07:47:45 +0600

    functional part complete

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -17,7 +17,7 @@ const Home = () => {
 
     const handleCart = (cart) => {
         const isExits = selectCourse.find(item => item.id == cart.id)
-        const count = cart.credit
+        let count = cart.credit
 
         if (isExits) {
             return alert('you connot select multiput course')
@@ -41,7 +41,7 @@ const Home = () => {
 
     return (
         <div className="flex gap-5 p-0 mt-5 w-11/12">
-            <div className="grid grid-cols-3  gap-4 mx-auto">
+            <div className="grid grid-cols-3  gap-8 mx-auto">
                 {
                     carts.map(cart => <Cart key={cart.id} handleCart={handleCart} cart={cart}></Cart>)
                 }
